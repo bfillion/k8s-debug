82d7f2d0b38e371532f11893dceb513b98dedf15
Bruno Fillion <brunofillion@Mac-Pro-de-Bruno.local>
2019-11-28 20:32:11 -0500

    healthcheck

--- kafka.ui/kafka.ui/Program.cs
+++ kafka.ui/kafka.ui/Program.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace kafka.ui
 {
@@ -13,11 +15,18 @@ namespace kafka.ui
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- kafka.ui/kafka.ui/Startup.cs
+++ kafka.ui/kafka.ui/Startup.cs
@@ -2,7 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Confluent.Kafka;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -20,15 +25,35 @@ namespace kafka.ui
 
         public IConfiguration Configuration { get; }
 
+        public ILifetimeScope AutofacContainer { get; private set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var configuration = new ProducerConfig()
+            {
+                BootstrapServers = "kafka:29092",
+                MessageSendMaxRetries = 0
+            };
+
+            services.AddHealthChecks()
+                .AddKafka(configuration, tags: new string[] { "kafka" });
+
+            services.AddHealthChecksUI();
+
             services.AddControllersWithViews();
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            //builder.RegisterModule(new AutofacModule());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            this.AutofacContainer = app.ApplicationServices.GetAutofacRoot();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -37,7 +62,17 @@ namespace kafka.ui
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            
+
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                Predicate = _ => true
+            })
+            .UseHealthChecks("/healthz", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -49,6 +84,8 @@ namespace kafka.ui
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapHealthChecksUI();
             });
         }
     }
